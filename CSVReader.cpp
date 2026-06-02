408bec24a7cece03e713fd915cb8653d7fe87588
dejagraver <dejaamaartin@gmail.com>
2026-06-02 08:21:54 +0300

    Developed OrderBook to take a vector of strings which will be read line by line, added in functionality to throw an excetpion for unexpected input so the program doesnt close out/crash, converted strings into doubles, 'OrderBookEntry::stringToOrderBookType(tokens[2])' allows us to translate the string into an order book type so the appropriate data can be passed for the 5th argument, which is the type (bid/ask/unknown)

--- CSVReader.cpp
+++ CSVReader.cpp
@@ -1,4 +1,7 @@
 #include "CSVReader.h"
+#include <iostream>
+#include <vector>
+#include "OrderBookEntry.h"
 CSVReader::CSVReader()
 {
 
@@ -14,15 +17,40 @@ std::vector<std::string> CSVReader::tokenise(std::string csvLine, char separator
     std::vector<std::string> tokens;
     return tokens;
 }
-OrderBookEntry CSVReader::stringToOrderBookEntry(std::vector<std::string> strings)
+OrderBookEntry CSVReader::stringToOrderBookEntry(std::vector<std::string> tokens)
 {
-    return OrderBookEntry{
-        0, 
-        0, 
-        "", 
-        "", 
-        OrderBookType::bid
-    };
-    return OrderBookEntry{0, 0, "", "", OrderBookType::bid};
+    double price, amount;
+     if (tokens.size() != 5) //bad
+            {
+                std::cout << "Invalid line: " << std::endl;
+                throw std::exception{};
+            }
+       
+
+    
+            try {
+                price = std::stod(tokens[3]);
+                amount = std::stod(tokens[4]);
+                std::cout << "Price: " << price << " Amount: " << amount << std::endl;
+
+            } catch (const std::exception& e) {
+                std::cout << "Invalid number in line due to a bad float: " << tokens[3] << std::endl;
+                std::cout << "Invalid number in line due to a bad float: " << tokens[4] << std::endl;
+                throw;
+            }
+
+            for (const std::string& t : tokens)
+            {
+                std::cout << t << std::endl;
+            }
+            std::cout << "==============" << std::endl;
+            OrderBookEntry{
+                price, 
+                amount, 
+                tokens[0], // timestamp represented by tokens[0] because the first column in the csv file is the timestamp
+                tokens[1], // product represented by tokens[1] because the second column in the csv file is the product
+                OrderBookEntry::stringToOrderBookType(tokens[2]) // order type represented by tokens[2] because the third column in the csv file is the order type
+            };
+            return OrderBookEntry{0, 0, "", "", OrderBookType::bid};
 }
 
--- OrderBookEntry.cpp
+++ OrderBookEntry.cpp
@@ -13,3 +13,21 @@ OrderBookEntry::OrderBookEntry( double _price,
 {
     
 }
+
+// in the cpp file we dont need to repeat the static keyword because we are not defining the function here we are just declaring it. 
+// We only need to use the static keyword in the header file when we are declaring the function. 
+// In the cpp file we just need to define the function without the static keyword.
+// namespace it with "orderBookEntry::" to show that this function belongs to the OrderBookEntry class. 
+// We know the function belongs to the OrderBookEntry class because we declared it in the header file as a static function.
+OrderBookType OrderBookEntry::stringToOrderBookType(std::string s)
+{
+   if ( s == "ask")
+   {
+       return OrderBookType::ask;
+   }
+   if ( s == "bid")
+   {
+       return OrderBookType::bid;
+   }
+  return OrderBookType::unknown;
+}
